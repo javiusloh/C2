f1cbab77a1fa3394af1616d8e64c72131cc59fa7
Javius Loh Jingwei <145333430+javiusloh@users.noreply.github.com>
2024-07-20 10:18:31 +0800

    Initial Commit

---
+++ C2/C2App.swift
@@ -0,0 +1,17 @@
+//
+//  C2App.swift
+//  C2
+//
+//  Created by Javius Loh on 20/7/24.
+//
+
+import SwiftUI
+
+@main
+struct C2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
