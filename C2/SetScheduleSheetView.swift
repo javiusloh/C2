699d5a0bb8f4f94fdb83eb310e6c9404aaddb3a9
ZenTan <zentan2801@gmail.com>
2024-07-20 12:31:59 +0800

    Added Halfway Done Schedule View (scrapped schedule view but still save it first, in case)

---
+++ C2/SetScheduleSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  SetScheduleSheetView.swift
+//  C2
+//
+//  Created by T Krobot on 20/7/24.
+//
+
+import SwiftUI
+
+struct SetScheduleSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetScheduleSheetView()
+}
