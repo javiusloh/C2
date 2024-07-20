c29808f18162da0f1646ee24ac43c1a045ef232f
Javius Loh Jingwei <145333430+javiusloh@users.noreply.github.com>
2024-07-20 10:24:15 +0800

    Updated with Different SwiftUI files

---
+++ C2/ScheduleView.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleView.swift
+//  C2
+//
+//  Created by Javius Loh on 20/7/24.
+//
+
+import SwiftUI
+
+struct ScheduleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScheduleView()
+}
---
+++ C2/ShopView.swift
@@ -0,0 +1,18 @@
+//
+//  ShopView.swift
+//  C2
+//
+//  Created by Javius Loh on 20/7/24.
+//
+
+import SwiftUI
+
+struct ShopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShopView()
+}
